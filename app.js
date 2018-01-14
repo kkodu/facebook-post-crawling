c8419efd3522f46413d5d74be323babcd43978e9
kkodu <nawy1130@gmail.com>
2018-01-14 23:53:30 +0900

    using prototype ing...

--- app.js
+++ app.js
@@ -5,44 +5,18 @@ app.listen(3000, function(req, res) {
   console.log("Server starts on port 3000");
 });
 
-/* 이벤트 설정 */
-var EventEmitter = require('events');
+var EVENT = require('events');
 var util = require('util');
-var url = require('url');
 
-// 추가 피드 요청 이벤트 에미터
-function nextPostEmitter() {
-	EventEmitter.call(this);
+function eventEmitter() {
+  EVENT.call(this);
 }
-util.inherits(nextPostEmitter, EventEmitter);
+util.inherits(eventEmitter, EVENT);
 
-// 몽구스 접속 후 페이스북 토큰 요청 이벤트 에미터
-function nextMongoEmitter() {
-  EventEmitter.call(this);
-}
-util.inherits(nextMongoEmitter, EventEmitter);
+var nextEmitter = new eventEmitter();
 
-var npEmitter = new nextPostEmitter();
-var nmEmitter = new nextMongoEmitter();
-
-/* 몽구스 설정 */
-var mongoose = require('mongoose');
-var mg_config = require('./config/mg-config.json');
-var MODEL = require('./model/fbpostmodel.js');
-mongoose.Promise = global.Promise;
-
-var conn = mongoose.connection;
-conn.on("error", console.error.bind(console, "mongoose connection error! --"));
-conn.openUri(`mongodb://${mg_config.userId}:${mg_config.userPass}@${mg_config.userLocal}/${mg_config.db}`);
-conn.once("open", function() {
-  console.log("mongoose successfully connected @");
-  nmEmitter.emit('req-fb-access', "event request received.. starts getting facebook access token");
-});
-
-
-/* 페이스북 설정 */
-var FB = require('fb'); // 페이스북 API 모듈
-var fb_config = require('./config/fb-config.json'); // 페이스북 개발자 접근 권한 환경 변수
+var fb_config = require('./config/fb-config.json');
+var FacebookPost = require('./src/fb-post');
 
 var pageLink = "973432719345219"; // 크롤링 하려는 공개 페이지 토큰
 var args = {
@@ -51,78 +25,5 @@ var args = {
   limit: 10
 };
 
-// 접근 권한 토큰 생성 함수
-var getAccessToken = function() {
-  // ES6 Promise 사용
-  return new Promise(function(resolve, reject) {
-    // 토큰 생성 => facebook.api(url, args, callback function)
-    FB.api('oauth/access_token', {
-      client_id: fb_config.clientId,
-      client_secret: fb_config.clientSecret,
-      grant_type: fb_config.grantType
-    },
-    // 응답 콜백
-    function(response) {
-      if(response.error)
-        reject(response.error); // 요청 오류
-      else
-        resolve(response.access_token); // 응답 액세스 토큰
-    });
-  });
-};
-
-// limit request count
-var lrc = (function() {
-  var count = 0;
-  return count;
-}());
-
-var getPagePosts = function(link, args) {
-  if(lrc > 2) process.exit(1);
-
-  // 해당 페이지 게시물 수집
-  FB.api(link + '/posts', 'get', args, function(res) {
-    if(res.error) {
-      console.log(res.error);
-      process.exit(1); // 프로세스 종료
-    }
-
-    var data = res.data; // 요청 data
-    console.log(`\n[${lrc}]-----------------------------------------------------------\n`, data);
-
-    // 다음 피드가 있는 경우
-    if(res.paging && res.paging.next !== undefined) {
-      var nextLinkParts = url.parse(res.paging.next, true); // url 파싱을 통한 다음 포스트 토큰과 액세스 토큰 분리
-      var nextArgs = {
-        link: link,
-        args: args
-      };
-      nextArgs.args.after = nextLinkParts.query.after;
-      nextArgs.args.access_token = nextLinkParts.query.access_token;
-      lrc++;
-
-      npEmitter.emit('event', nextArgs); // 이벤트를 통한 다음 요청 전달
-    }
-  });
-};
-
-// 추가 피드 요청 이벤트 응답
-npEmitter.on('event', function(req) {
-	console.log('------------------------------------------------------------------');
-  getPagePosts(req.link, req.args); // 다음 요청 실행
-});
-
-// 몽구스 접속 후 이벤트 발생을 통한 페이스북 크롤링
-nmEmitter.on('req-fb-access', function(msg) {
-  console.log(msg);
-  // Promise 실행
-  getAccessToken().then(
-    function(accessToken) {
-      FB.setAccessToken(accessToken) // 동기 처리
-      console.log("Access Token set")
-      getPagePosts(pageLink, args); // 포스트 요청
-    },
-    function(error) {
-      console.log(error);
-    });
-});
+var fbposts = new FacebookPost(pageLink, args, nextEmitter);
+fbposts.getAccessToken(fb_config);
--- src/fb-post.js
+++ src/fb-post.js
@@ -2,3 +2,73 @@ var FB = require('fb'),
     EVENT = require('events'),
     util = require('util'),
     url = require('url');
+
+function FacebookPost(link, args, nextEmitter) {
+  this.link = link;
+  this.args = args;
+  this.nm = nextEmitter;
+
+  this.nm.on("access-error", function(error) {
+    console.log(error);
+    process.exit(1);
+  });
+
+  this.nm.on("access-ok", function(access_token, this_post) {
+    FB.setAccessToken(access_token);
+    console.log("Access Token set");
+    this_post.__proto__.getPagePosts(this_post.link, this_post.args); // 포스트 요청
+  });
+
+  this.nm.on('next-post', function(req, this_post) {
+  	console.log('------------------------------------------------------------------');
+    this_post.__proto__.getPagePosts(req.link, req.args); // 다음 요청 실행
+  });
+
+}
+
+FacebookPost.prototype.getAccessToken = function(fb_config) {
+  var this_post = this;
+  var nextEmitter = this.nm;
+  FB.api('oauth/access_token', {
+    client_id: fb_config.clientId,
+    client_secret: fb_config.clientSecret,
+    grant_type: fb_config.grantType
+  },
+  function(response) {
+    if(response.error)
+      nextEmitter.emit("access-error", response.error);
+    else
+      nextEmitter.emit("access-ok", response.access_token, this_post);
+  });
+}
+
+FacebookPost.prototype.getPagePosts = function(link, args) {
+  var this_post = this;
+  var nextEmitter = this.nm;
+  console.log(this);
+  console.log(nextEmitter);
+  FB.api(link + '/posts', 'get', args, function(res) {
+    if(res.error) {
+      console.log(res.error);
+      process.exit(1); // 프로세스 종료
+    }
+
+    var data = res.data; // 요청 data
+    console.log(data);
+
+    // 다음 피드가 있는 경우
+    if(res.paging && res.paging.next !== undefined) {
+      var nextLinkParts = url.parse(res.paging.next, true); // url 파싱을 통한 다음 포스트 토큰과 액세스 토큰 분리
+      var nextArgs = {
+        link: link,
+        args: args
+      };
+      nextArgs.args.after = nextLinkParts.query.after;
+      nextArgs.args.access_token = nextLinkParts.query.access_token;
+
+      nextEmitter.emit('next-post', nextArgs, this_post); // 이벤트를 통한 다음 요청 전달
+    }
+  });
+}
+
+module.exports = FacebookPost;
