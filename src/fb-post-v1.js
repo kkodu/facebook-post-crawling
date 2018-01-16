ae3f5efeaa2a0d2c900818f4eb81c92341f5a5eb
kkodu <nawy1130@gmail.com>
2018-01-17 02:41:28 +0900

    add and modify next args function

---
+++ src/fb-post-v1.js
@@ -0,0 +1,115 @@
+var FB = require('fb'),
+    EVENT = require('events'),
+    util = require('util'),
+    url = require('url');
+
+function FacebookPost(link, args) {
+  this.link = link;
+  this.args = args;
+  this.nm = (function() {
+    var eventEmitter = function() {
+      EVENT.call(this);
+    }
+    util.inherits(eventEmitter, EVENT);
+
+    return new eventEmitter();
+  }());
+
+  this.setArgs = function(next_args) {
+    this.args = next_args.args;
+  }
+
+  this.nm.on('access-error', function(error) {
+    console.log(error);
+    process.exit(1);
+  });
+
+  this.nm.on('access-ok', function(access_token, this_post) {
+    FB.setAccessToken(access_token);
+    console.log("Access Token set");
+    this_post.__proto__.getPagePosts(this_post); // 포스트 요청
+  });
+
+  this.nm.on('next-post', function(this_post) {
+    this_post.__proto__.getPagePosts(this_post); // 다음 요청 실행
+  });
+
+  this.nm.on('data-obj', function(data, this_post) {
+    if(!Array.isArray(data)) {
+      console.log(">> end point of data!!");
+      process.exit(1);
+    } else {
+      for(index in data) {
+        var id = data[index].id;
+        this_post.__proto__.getPostSub(id, this_post);
+      }
+    }
+  });
+}
+
+FacebookPost.prototype.accessPage = function(fb_config) {
+  var this_post = this;
+  var nextEmitter = this.nm;
+  FB.api('oauth/access_token', {
+    client_id: fb_config.clientId,
+    client_secret: fb_config.clientSecret,
+    grant_type: fb_config.grantType
+  },
+  function(response) {
+    if(response.error)
+      nextEmitter.emit('access-error', response.error);
+    else
+      nextEmitter.emit('access-ok', response.access_token, this_post);
+  });
+};
+
+FacebookPost.prototype.getPagePosts = function(this_post) {
+  var _this = this_post;
+  var nextEmitter = _this.nm;
+  FB.api(_this.link + '/posts', 'get', _this.args, function(res) {
+    if(res.error) {
+      console.log(res.error);
+      process.exit(1); // 프로세스 종료
+    }
+
+    var data = res.data; // 요청 data
+    console.log("[page post]------------------------------------------------------------------");
+    console.log(data);
+    console.log("-----------------------------------------------------------------------------");
+
+    // 데이터 묶음을 전송하여 각 게시물의 좋아요, 댓글, 반응을 호출하도록 유도
+    nextEmitter.emit('data-obj', data, this_post);
+
+    // 다음 피드가 있는 경우
+    if(res.paging && res.paging.next !== undefined) {
+      var nextLinkParts = url.parse(res.paging.next, true); // url 파싱을 통한 다음 포스트 토큰과 액세스 토큰 분리
+      var nextArgs = {
+        args: _this.args
+      };
+      nextArgs.args.after = nextLinkParts.query.after;
+      nextArgs.args.access_token = nextLinkParts.query.access_token;
+      _this.setArgs(nextArgs);
+
+      nextEmitter.emit('next-post', _this); // 이벤트를 통한 다음 요청 전달
+    }
+  });
+};
+
+FacebookPost.prototype.getPostSub = function(id, this_post) {
+  var typeSet = ["likes", "comments", "reactions"];
+  for(index in typeSet)
+    this_post.__proto__.reqFbApi(id, typeSet[index], this_post);
+};
+
+FacebookPost.prototype.reqFbApi = function(id, type, this_post) {
+  FB.api(id + '/' + type, 'get', function(res) {
+    if(res.error) { console.log(res.error); process.exit(1); }
+
+    console.log("[post " + type + "]-----------------------------------------------------------------");
+    if(res.data) console.log(res.data);
+    else console.log("cannot find " + type);
+    console.log("-----------------------------------------------------------------------------");
+  });
+}
+
+module.exports = FacebookPost;
--- src/fb-post.js
+++ src/fb-post.js
@@ -15,41 +15,51 @@ function FacebookPost(link, args) {
     return new eventEmitter();
   }());
 
-  this.setArgs = function(next_args) {
-    this.args = next_args.args;
-  }
+  this.getNextArgs = function(link) {
+    var nextLinkParts = url.parse(link, true), // url 파싱을 통한 다음 포스트 토큰과 액세스 토큰 분리
+        nextArgs = {
+          args: this.args
+        };
+    nextArgs.args.after = nextLinkParts.query.after;
+    nextArgs.args.access_token = nextLinkParts.query.access_token;
+
+    return nextArgs;
+  };
 
   this.nm.on('access-error', function(error) {
     console.log(error);
     process.exit(1);
   });
 
-  this.nm.on('access-ok', function(access_token, this_post) {
+  this.nm.on('access-ok', function(access_token, _this) {
     FB.setAccessToken(access_token);
     console.log("Access Token set");
-    this_post.__proto__.getPagePosts(this_post); // 포스트 요청
+    _this.__proto__.getPagePosts(_this); // 포스트 요청
   });
 
-  this.nm.on('next-post', function(this_post) {
-    this_post.__proto__.getPagePosts(this_post); // 다음 요청 실행
+  this.nm.on('next-args', function(set, _this) {
+    var args = _this.getNextArgs(set.nextLink);
+    set.next = args;
+
+    _this.__proto__.getNextFeed(set, _this);
   });
 
-  this.nm.on('data-obj', function(data, this_post) {
+  this.nm.on('data-obj', function(data, _this) {
     if(!Array.isArray(data)) {
       console.log(">> end point of data!!");
       process.exit(1);
     } else {
-      for(index in data) {
+      for(var index in data) {
         var id = data[index].id;
-        this_post.__proto__.getPostSub(id, this_post);
+        _this.__proto__.getPostSub(id, _this);
       }
     }
   });
 }
 
 FacebookPost.prototype.accessPage = function(fb_config) {
-  var this_post = this;
-  var nextEmitter = this.nm;
+  var _this = this;
+  var nextEmitter = _this.nm;
   FB.api('oauth/access_token', {
     client_id: fb_config.clientId,
     client_secret: fb_config.clientSecret,
@@ -59,57 +69,52 @@ FacebookPost.prototype.accessPage = function(fb_config) {
     if(response.error)
       nextEmitter.emit('access-error', response.error);
     else
-      nextEmitter.emit('access-ok', response.access_token, this_post);
+      nextEmitter.emit('access-ok', response.access_token, _this);
   });
 };
 
-FacebookPost.prototype.getPagePosts = function(this_post) {
-  var _this = this_post;
+FacebookPost.prototype.getPagePosts = function(_this) {
+  _this.__proto__.reqFbApi(_this.link, 'posts', _this.args, _this);
+};
+
+FacebookPost.prototype.getPostSub = function(id, _this) {
+  // likes의 경우 reactions에 포함이 되므로 현재 생략
+  var typeSet = ["comments", "reactions"];
+  for(var index in typeSet)
+    _this.__proto__.reqFbApi(id, typeSet[index], _this.agrs, _this);
+};
+
+FacebookPost.prototype.getNextFeed = function(set, _this) {
+  if(set["type"] === 'posts')
+    _this.__proto__.reqFbApi(_this.link, set.type, set.next.args, _this);
+  else
+    _this.__proto__.reqFbApi(set.token, set.type, set.next.args, _this);
+};
+
+FacebookPost.prototype.reqFbApi = function(token, type, args, _this) {
   var nextEmitter = _this.nm;
-  FB.api(_this.link + '/posts', 'get', _this.args, function(res) {
-    if(res.error) {
-      console.log(res.error);
-      process.exit(1); // 프로세스 종료
-    }
+  FB.api(token + '/' + type, 'get', args, function(res) {
+    if(res.error) { console.log(res.error); process.exit(1); }
+    console.log(token + ' :: ' + type);
+    // console.log("[type: " + type + "]-----------------------------------------------------------------");
+    // if(res.data) console.log(res.data);
+    // else console.log("cannot find " + type);
+    // console.log("-----------------------------------------------------------------------------");
 
-    var data = res.data; // 요청 data
-    console.log("[page post]------------------------------------------------------------------");
-    console.log(data);
-    console.log("-----------------------------------------------------------------------------");
-
-    // 데이터 묶음을 전송하여 각 게시물의 좋아요, 댓글, 반응을 호출하도록 유도
-    nextEmitter.emit('data-obj', data, this_post);
+    if(type === 'posts')
+      nextEmitter.emit('data-obj', res.data, _this);
 
     // 다음 피드가 있는 경우
     if(res.paging && res.paging.next !== undefined) {
-      var nextLinkParts = url.parse(res.paging.next, true); // url 파싱을 통한 다음 포스트 토큰과 액세스 토큰 분리
-      var nextArgs = {
-        args: _this.args
+      var set = {
+        nextLink: res.paging.next,
+        type: type,
       };
-      nextArgs.args.after = nextLinkParts.query.after;
-      nextArgs.args.access_token = nextLinkParts.query.access_token;
-      _this.setArgs(nextArgs);
+      if(type !== 'posts') set.token = token;
 
-      nextEmitter.emit('next-post', _this); // 이벤트를 통한 다음 요청 전달
+      nextEmitter.emit('next-args', set, _this);
     }
   });
 };
-
-FacebookPost.prototype.getPostSub = function(id, this_post) {
-  var typeSet = ["likes", "comments", "reactions"];
-  for(index in typeSet)
-    this_post.__proto__.reqFbApi(id, typeSet[index], this_post);
-};
-
-FacebookPost.prototype.reqFbApi = function(id, type, this_post) {
-  FB.api(id + '/' + type, 'get', function(res) {
-    if(res.error) { console.log(res.error); process.exit(1); }
-
-    console.log("[post " + type + "]-----------------------------------------------------------------");
-    if(res.data) console.log(res.data);
-    else console.log("cannot find " + type);
-    console.log("-----------------------------------------------------------------------------");
-  });
-}
 
 module.exports = FacebookPost;
