74bba7d1f4c34acc546531b4c566b1aa083fee0e
kkodu <nawy1130@gmail.com>
2018-01-19 23:49:38 +0900

    limit .

--- app.js
+++ app.js
@@ -11,7 +11,8 @@ var FacebookPost = require('./src/fb-post-v2');
 var pageLink = "973432719345219"; // 크롤링 하려는 공개 페이지 토큰
 var args = {
   // 가져올 데이터 설정
-  fields: ['id', 'from', 'message', 'link', 'created_time', 'full_picture', 'source']
+  fields: ['id', 'from', 'message', 'link', 'created_time', 'full_picture', 'source'],
+limit: 10
 };
 
 var fbposts = new FacebookPost(pageLink, args);
